bb4e00e994e574bbd50c0229b6542281a81d0928
Margherita <mangoguglia@gmail.com>
2020-07-13 17:24:49 +0200

    js-html-css-boolzap

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready (function(){
+
+// uso del tasto invio
+$('input').keydown(function(event){
+   if(event.which==13 || event.keyCode==13){
+   sendMessage();
+ }
+});
+
+// Funzioni
+
+function sendMessage (){
+ var contenutoMessaggio = $('.container-msg > input').val();
+ console.log(contenutoMessaggio);
+ var transform = $(contenutoMessaggio).clone();
+ var oneChat = $(this);
+ var invioMessaggio = $('#chat');
+
+  invioMessaggio.append(contenutoMessaggio);
+}
+});
