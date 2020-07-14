b4ce0b5755efbe394b9efb7256f088f3710073d6
Margherita <mangoguglia@gmail.com>
2020-07-14 21:20:13 +0200

    js-html-css-boolzap/chiedigli come sta

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
-$(document).ready (function(){
+function addSendListener (){
 
-// uso del tasto invio
-$('input').keydown(function(event){
-   if(event.which==13 || event.keyCode==13){
-   sendMessage();
- }
-});
+var target = $('#message-input')
+target.keyup(sendKeyup);
 
-// Funzioni
-
-function sendMessage (){
- var contenutoMessaggio = $('.container-msg > input').val();
- console.log(contenutoMessaggio);
- var template = $('.template p').clone();
-
- var invioMessaggio = $('#message-content');
- template.append(contenutoMessaggio);
-
-  invioMessaggio.append(template);
 }
-});
+// funzione 1
+function sendKeyup(event){
+
+  var key = event.which;
+  var input = $(this);
+  var txt = input.val();
+
+
+  if(key === 13 && txt.length>0){
+
+    input.val('');
+
+    sendMessage(txt);
+
+    setTimeout(function() {
+    // tempo due secondi in cui deve partire la risposta
+    receivedMessage('Come dice Giovanni: "sono contentino.."');
+   },2000);
+  }
+}
+// funzione 2
+function sendMessage(txt){
+  var template = $('#sent').clone();
+  var target = $('#right-messages');
+
+
+  template.find('#msg-text').text(txt);
+  template.find('#msg-time').text(getActualHour());
+
+
+  target.append(template);
+}
+// funzione 3
+function getActualHour(){
+  var date = new Date();
+  return date.getHours() + ':' + date.getMinutes();
+}
+
+// funzione 4
+function receivedMessage(txt){
+  var template = $('#received').clone();
+  var target = $('#right-messages');
+
+
+  template.find('#msg-text-received').text(txt);
+  template.find('#msg-time-received').text(getActualHour());
+
+
+  target.append(template);
+}
+
+
+
+// Funzioni di collegamento
+function init () {
+  addSendListener();
+}
+
+$(document).ready(init);
